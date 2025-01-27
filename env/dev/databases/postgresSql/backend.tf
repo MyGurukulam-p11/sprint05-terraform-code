97558bcec77c739fb5b047c0e8428e6e53cd0d71
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-01-27 15:58:24 +0530

    Update backend.tf

---
+++ env/dev/databases/postgresSql/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "combine-force-p11-statefile"
+    key     = "env/dev/databases/postgresSql/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
+}
