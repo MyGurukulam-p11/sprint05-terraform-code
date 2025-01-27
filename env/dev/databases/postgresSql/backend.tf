b3fb65dc3147a3357bc8a4a326f0973132699e71
amitnagar1300 <amit.nagar.snaatak@mygurukulam.co>
2025-01-27 23:12:28 +0530

    Update backend.tf

--- env/dev/databases/postgresSql/backend.tf
+++ env/dev/databases/postgresSql/backend.tf
@@ -7,12 +7,3 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "network_skeleton_state" {
-  backend = "s3"
-
-  config = {
-    bucket = "combine-force-p11-statefile"
-    key    = "env/dev/network_skeleton/module/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
