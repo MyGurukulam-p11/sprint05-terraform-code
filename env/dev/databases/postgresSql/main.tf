068332a62c29588130938cf334d366e65bf63f29
amitnagar1300 <amit.nagar.snaatak@mygurukulam.co>
2025-01-27 23:47:56 +0530

    Update main.tf

--- env/dev/databases/postgresSql/main.tf
+++ env/dev/databases/postgresSql/main.tf
@@ -60,5 +60,4 @@ resource "aws_route53_record" "a_record" {
   name    = "db.com"         # Fully qualified domain name
   type    = "A"
   ttl     = 300
-  records = [module.postgresSQL.db_server_id] # Wrap the string in a list
-}
+  records = [module.postgresSQL.db_server_ip_address] 
