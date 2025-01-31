f47b14673e29da96a51b421fbaae5ec9116f21ff
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-01-31 23:36:23 +0530

    Update main.tf

--- env/dev/application/frontend/main.tf
+++ env/dev/application/frontend/main.tf
@@ -1,5 +1,5 @@
 module "asg" {
-  source = "https://github.com/MyGurukulam-p11/sprint05-terraform-code/blob/amit_scrum_220/env/dev/application/frontend/main.tf"
+  source = "git::git@github.com:MyGurukulam-p11/sprint05-terraform-code.git//module/asg?ref=pritam_scrum_208"
   tcp_protocol = var.tcp_protocol
   application_port = var.application_port
   environment = var.environment
