443c769d5cc1ea166cde418ca4e6aafe838d995c
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-02-01 00:06:27 +0530

    Delete env/dev/application/attendance/backend.tf

--- env/dev/application/attendance/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "otms-p11-dev-terraform-statefile"
-    key     = "env/dev/application/attendance/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
-  }
-}
