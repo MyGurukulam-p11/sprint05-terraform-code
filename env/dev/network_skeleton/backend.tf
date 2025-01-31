a0651b10c44cc8040485d4583c92ac3fbafe1f01
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-01-31 22:35:52 +0530

    Delete env/dev/network_skeleton/backend.tf

--- env/dev/network_skeleton/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "infinity-p11-terraform-state"   
-    key            = "env/dev/network_skeleton/module/terraform.tfstate"  
-    region         = "ap-south-1"                     
-  }
-}
