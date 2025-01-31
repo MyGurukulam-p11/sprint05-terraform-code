a7e526a077fd91c6b162960c719e1e7835587453
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-01-31 22:57:25 +0530

    Delete env/dev/databases/scyllaDb/backend.tf

--- env/dev/databases/scyllaDb/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "infinity-p11-terraform-state"
-    key     = "env/dev/databases/scyllaDb/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
-  }
-}
