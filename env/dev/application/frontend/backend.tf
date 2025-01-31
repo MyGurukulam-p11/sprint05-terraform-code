d0c24fca5e61467f307140a4bdc1d6043f936109
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-01-31 22:41:44 +0530

    Delete env/dev/application/frontend/backend.tf

--- env/dev/application/frontend/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "infinity-p11-terraform-state"
-    key     = "env/dev/application/frontend/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
-  }
-}
