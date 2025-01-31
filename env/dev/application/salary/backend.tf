ae2e413530a7dbe384db473d14f5c6a6aa269ff7
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-01-31 22:46:02 +0530

    Delete env/dev/application/salary/backend.tf

--- env/dev/application/salary/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "combine-force-p11-statefile"
-    key     = "env/dev/application/salary/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
-  }
-}
