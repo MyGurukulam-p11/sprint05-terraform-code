410ed6004bc1021bf49cfe9db82dd5f6b0acc71e
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-02-12 20:48:31 +0530

    Update backend.tf

--- env/dev/network_skeleton/backend.tf
+++ env/dev/network_skeleton/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "otms-p11-dev-terraform-statefile"   
+    bucket         = "demo-p11-terraform-statefile"   
     key            = "env/dev/network_skeleton/module/terraform.tfstate"  
     region         = "ap-south-1"                     
   }
