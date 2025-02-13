a26bcf0e6f1af1c568db286d40bbffb5016f8244
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-02-13 14:17:04 +0530

    Update local.tf

--- module/asg/local.tf
+++ module/asg/local.tf
@@ -1,5 +1,22 @@
+
 # security group
 
 locals {
   sg_name = "${var.environment}-${var.application_name}-sg"
+}
+
+locals {
+  launch_template_name = "${var.environment}-${var.application_name}-launch-template"
+}
+
+locals {
+  tg_name = "${var.environment}-${var.application_name}-tg"
+}
+
+locals {
+  asg_name = "${var.environment}-${var.application_name}-asg"
+}
+
+locals {
+  instance_name = "${var.environment}-${var.application_name}-instance"
 }
