00f692f53bde3358030b4bbd81f5d835359906da
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-02-13 15:17:13 +0530

    Update backend.tf

--- env/dev/application/salary/backend.tf
+++ env/dev/application/salary/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "otms-p11-dev-terraform-statefile"
+    bucket  = "my-gurukulam-p11-statefile"
     key     = "env/dev/application/salary/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
