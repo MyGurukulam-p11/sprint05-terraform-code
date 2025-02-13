1aa3c0e74e00b643eed7cc0ef48d9fb1643e9854
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-02-13 15:17:37 +0530

    Update main.tf

--- env/dev/application/salary/main.tf
+++ env/dev/application/salary/main.tf
@@ -1,4 +1,4 @@
-module "salary_application" {
+module "employee_application" {
   source = "git::git@github.com:MyGurukulam-p11/sprint05-terraform-code.git//module/asg?ref=pritam_scrum_208"
   tcp_protocol = var.tcp_protocol
   application_port = var.application_port
@@ -8,21 +8,18 @@ module "salary_application" {
   instance_type = var.instance_type
   is_frontend = var.is_frontend
   key_name = var.key_name
-  launch_template_name = var.launch_template_name
   http_protocol = var.http_protocol
   interval = var.interval
   health_check_path = var.health_check_path
   timeout = var.timeout
   healthy_threshold = var.healthy_threshold
   unhealthy_threshold = var.unhealthy_threshold
-  tg_name = var.tg_name 
   http_listener_port = var.http_listener_port
   enable_http_listener = var.enable_http_listener
-  asg_name = var.asg_name
+  https_protocol = var.https_protocol
   desired_capacity = var.desired_capacity
   max_size = var.max_size
   min_size = var.min_size
-  instance_name = var.instance_name
   policy_name = var.policy_name
   policy_type = var.policy_type
   metric_type = var.metric_type
