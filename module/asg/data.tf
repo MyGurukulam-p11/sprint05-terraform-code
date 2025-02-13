0d3b96008b0c699639e33b74855a97f43642d388
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-02-13 14:16:42 +0530

    Update data.tf

--- module/asg/data.tf
+++ module/asg/data.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "network_skeleton_state" {
   backend = "s3"
 
   config = {
-    bucket = "otms-p11-dev-terraform-statefile"
+    bucket = "my-gurukulam-p11-statefile"
     key    = "env/dev/network_skeleton/module/terraform.tfstate"
     region = "ap-south-1"
   }
